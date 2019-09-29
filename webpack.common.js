e31845fd7d2a16834c5062d79d838d0700ef636b
Ahmet Simsek <iamaroott@gmail.com>
2019-09-29 13:55:01 +0300

    CopyWebpackPlugin added

--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,46 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/index.js', './src/index.css'
-  ],
+  entry: ['./src/index.js', './src/index.css'],
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({title: 'case challenge production', template: 'template/index.ejs'})
+    new HtmlWebpackPlugin({
+      title: 'case challenge production',
+      template: 'template/index.ejs',
+    }),
+    new CopyWebpackPlugin([{from: 'public/images', to: 'public/images'}]),
   ],
   module: {
     rules: [
       {
         test: /\.(jpg|png|gif)$/,
-        loader: 'file-loader'
-      }, {
+        loader: 'file-loader',
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }, {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader"
-          }, {
-            loader: "css-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build')
-  }
+    path: path.resolve(__dirname, 'build'),
+  },
 };
